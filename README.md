9c08c004ccc1b3f08941511a23820fb4e6b0e48a
etisdew <26851121+etisdew@users.noreply.github.com>
2018-03-03 03:14:09 -0800

    typical

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ Complete the following problems:
     to decimal: 207
 
 * Convert `4C` hex
-    to binary: 1001100
+    to binary: 0b1001100
     to decimal: 76
 
 * Convert `68` decimal
-    to binary: 1000100
+    to binary: 0b1000100
     to hex: 44
 
 ## Architecture
--- cpu.js
+++ cpu.js
@@ -3,38 +3,47 @@
  */
 const fs = require('fs');
 const SP_ADDR = 7;
-const BT = {
-  // Instructions - Ref LS8-SP_ADDREC.md
-  ADD: 0b10101000, // :144 tested etisdew
-  AND: 0b10110011, // :155
-  CALL: 0b01001000, // :167
-  CMP: 0b10100000, // :182
-  DEC: 0b01111001, // :201 tested etisdew
-  DIV: 0b10101011, // :212 tested etisdew
-  HLT: 0b00000001, // :227
-  INC: 0b01111000, // :238 tested etisdew
-  INT: 0b01001010, // :249
-  IRET: 0b00001011, // :263
-  JEQ: 0b01010001, // :281
-  JGT: 0b01010100, // :292
-  JLT: 0b01010011, // :304
-  JMP: 0b01010000, // :316
-  JNE: 0b01010010, // :329
-  LD: 0b10011000, // :341
-  LDI: 0b10011001, // :354
-  MOD: 0b10101100, // :365 tested etisdew
-  MUL: 0b10101010, // :380 tested etisdew
-  NOP: 0b00000000, // :391
-  NOT: 0b01110000, // :402
-  OR: 0b10110001, // :413
-  POP: 0b01001100, // :425
-  PRA: 0b01000010, // :439
-  PRN: 0b01000011, // :453
-  PUSH: 0b01001101, // :467
-  RET: 0b00001001, // :482
-  ST: 0b10011010, // :495
-  SUB: 0b10101001, // :508 tested etisdew
-  XOR: 0b10110010 // :520
+const DEFAULT_OP_SET = {
+  // Instructions - Ref LS8-SP_ADDREC.md Starting on ALU...
+  // Test 0: Defined in ...
+   LD:  0b10011000, // :341
+  LDI:  0b10011001, // :354
+   ST:  0b10011010, // :495
+  // Test 1: Defined in ...
+  CMP:  0b10100000, // :182
+  // Test 2: Defined in ...
+  ADD:  0b10101000, // :144 tested etisdew
+  SUB:  0b10101001, // :508 tested etisdew
+  MUL:  0b10101010, // :380 tested etisdew
+  DIV:  0b10101011, // :212 tested etisdew
+  MOD:  0b10101100, // :365 tested etisdew
+  // Test 3: Defined in ...
+   OR:  0b10110001, // :413
+  XOR:  0b10110010, // :520
+  AND:  0b10110011, // :155
+  // Test 4: Defined in ...
+  PRA:  0b01000010, // :439
+  PRN:  0b01000011, // :453
+  // Test 5: Defined in ...
+ CALL:  0b01001000, // :167
+  INT:  0b01001010, // :249
+  POP:  0b01001100, // :425
+ PUSH:  0b01001101, // :467
+  // Test 6: Defined in ...
+  JMP:  0b01010000, // :316
+  JEQ:  0b01010001, // :281
+  JNE:  0b01010010, // :329
+  JLT:  0b01010011, // :304
+  JGT:  0b01010100, // :292
+  // Test 7: Defined in ...
+  NOT:  0b01110000, // :402
+  INC:  0b01111000, // :238 tested etisdew
+  DEC:  0b01111001, // :201 tested etisdew
+  // Test 8: Defined in ...
+  NOP:  0b00000000, // :391
+  HLT:  0b00000001, // :227
+  RET:  0b00001001, // :482
+ IRET:  0b00001011, // :263
 }
 
 
@@ -46,121 +55,88 @@ class CPU {
   constructor(ram = 256) {
     this.ram = ram;
     this.reg = new Array(8).fill(0); // General-purpose registers
-
+    
     // Special-purpose registers
     this.reg.PC = 0; // Program Counter
     this.reg.IR = 0; // Instruction Register
     this.reg[SP_ADDR] = 0xf4;
-    this.setupBranchTable();
+    this.flag = 0b000;
+    this.setupBranchTable(DEFAULT_OP_SET);
+    // console.log(this.BT)
   }
 
 	/**
 	 * Sets up the branch table
-     * @param {void} refactor to take BT
-	 */
-  setupBranchTable() {
-    let bt = {};
-    // I think there is an improvement to be made here to remove the
-    // multiple declarations.
-    bt[BT.ADD] = this.ADD.bind(this); // works
-    bt[BT.AND] = this.AND.bind(this); // works
+   * @param { Object } BT provide the binary dictionary for mapping to function calls.
+   * 
+   */
+  setupBranchTable(BT) {
+    const bt = {};
+    bt[BT.ADD]  = this.ADD.bind(this); // works
+    bt[BT.AND]  = this.AND.bind(this); // works
     bt[BT.CALL] = this.CALL.bind(this); // test
-    bt[BT.CMP] = this.CMP.bind(this); // works
-    bt[BT.DEC] = this.DEC.bind(this); // works
-    bt[BT.DIV] = this.DIV.bind(this); // works
-    bt[BT.HLT] = this.HLT.bind(this); // works
-    bt[BT.INC] = this.INC.bind(this); // works
-    // bt[BT.INT] = this.INT.bind(this); //    make
-    // bt[BT.IRET] = this.IRET.bind(this); //  make
-    bt[BT.JEQ] = this.JEQ.bind(this); //    make
-    bt[BT.JGT] = this.JGT.bind(this); //    make
-    bt[BT.JLT] = this.JLT.bind(this); //    make
-    bt[BT.JMP] = this.JMP.bind(this); //    make
-    bt[BT.JNE] = this.JNE.bind(this); //    make
-    // bt[BT.LD] = this.LD.bind(this); //    make
-    bt[BT.LDI] = this.LDI.bind(this); // works
-    bt[BT.MOD] = this.MOD.bind(this); // works
-    bt[BT.MUL] = this.MUL.bind(this); // works
-    bt[BT.NOP] = this.NOP.bind(this); // works
-    bt[BT.NOT] = this.NOT.bind(this); // works
-    bt[BT.OR] = this.OR.bind(this); // works
-    bt[BT.POP] = this.POP.bind(this); //    test
-    // bt[BT.PRA] = this.PRA.bind(this); //    make
-    bt[BT.PRN] = this.PRN.bind(this); // works
+    bt[BT.CMP]  = this.CMP.bind(this); // works
+    bt[BT.DEC]  = this.DEC.bind(this); // works
+    bt[BT.DIV]  = this.DIV.bind(this); // works
+    bt[BT.HLT]  = this.HLT.bind(this); // works
+    bt[BT.INC]  = this.INC.bind(this); // works
+    // bt[BT.INT]   = this.INT.bind(this); //    make
+    // bt[BT.IRET]  = this.IRET.bind(this); //  make
+    // bt[BT.JEQ]  = this.JEQ.bind(this); //    make
+    // bt[BT.JGT]  = this.JGT.bind(this); //    make
+    // bt[BT.JLT]  = this.JLT.bind(this); //    make
+    // bt[BT.JMP]  = this.JMP.bind(this); //    make
+    // bt[BT.JNE]  = this.JNE.bind(this); //    make
+    // bt[BT.LD]  = this.LD.bind(this); //    make
+    bt[BT.LDI]  = this.LDI.bind(this); // works
+    bt[BT.MOD]  = this.MOD.bind(this); // works
+    bt[BT.MUL]  = this.MUL.bind(this); // works
+    bt[BT.NOP]  = this.NOP.bind(this); // works
+    bt[BT.NOT]  = this.NOT.bind(this); // works
+    bt[BT.OR]   = this.OR.bind(this); // works
+    bt[BT.POP]  = this.POP.bind(this); //    test
+    // bt[BT.PRA]   = this.PRA.bind(this); //    make
+    bt[BT.PRN]  = this.PRN.bind(this); // works
     bt[BT.PUSH] = this.PUSH.bind(this); //    test
-    bt[BT.RET] = this.RET.bind(this); //    test
-    // bt[BT.ST] = this.ST.bind(this); //    make
-    bt[BT.SUB] = this.SUB.bind(this); // works
-    bt[BT.XOR] = this.XOR.bind(this); // works
-    // Establishes that this.branchTable is initialized with its op codes
-    // as keys and functions as value.
+    bt[BT.RET]  = this.RET.bind(this); //    test
+    // bt[BT.ST]  = this.ST.bind(this); //    make
+    bt[BT.SUB]  = this.SUB.bind(this); // works
+    bt[BT.XOR]  = this.XOR.bind(this); // works
     this.branchTable = bt;
   }
 
-  /**
-   * Store value in memory address, useful for program loading
-   * @param address Memory address
-   * @param value Value to place in memory address
-   */
-  poke(address, value) {
-    this.ram.write(address, value);
-  }
+  poke(address, value) { this.ram.write(address, value) }
+  startClock() { this.clock = setInterval(_ => { this.tick() }); }
+  stopClock() { clearInterval(this.clock) }
 
-  /**
-   * Starts the clock ticking on the CPU
-   */
-  startClock() {
-    const _this = this;
-
-    this.clock = setInterval(() => {
-      _this.tick();
-    }, 1);
-  }
-
-  /**
-   * stopClock
-   * calls clearInterval with this.clock
-   */
-  stopClock() {
-    clearInterval(this.clock);
-  }
-
-  /**
-   * ALU functionality
-   * 
-   * op can be: ADD SUB MUL DIV INC DEC CMP MOD
-   */
   alu(op, regA, regB) {
     switch (op) {
       case 'NOP': { break; }
+      
       case 'ADD': { this.reg[regA] += this.reg[regB]; break; }
       case 'SUB': { this.reg[regA] -= this.reg[regB]; break; }
       case 'MUL': { this.reg[regA] *= this.reg[regB]; break; }
       case 'DIV': { this.reg[regA] /= this.reg[regB]; break; }
       case 'MOD': { this.reg[regA] %= this.reg[regB]; break; }
+      
       case 'INC': { this.reg[regA] += 1; break; }
       case 'DEC': { this.reg[regA] -= 1; break; }
+      
+      case 'OR':  { this.reg[regA] |= this.reg[regB]; break; }
+      case 'XOR': { this.reg[regA] ^= this.reg[regB]; break; }
       case 'NOT': { this.reg[regA] = ~this.reg[regA]; break; }
       case 'AND': { this.reg[regA] &= this.reg[regB]; break; }
-      case 'OR': { this.reg[regA] |= this.reg[regB]; break; }
-      case 'XOR': { this.reg[regA] ^= this.reg[regB]; break; }
+      
+      case 'JEQ': (this.flag === 0b001);
+      case 'JGT': (this.flag === 0b010);
+      case 'JLT': (this.flag === 0b100);
+      case 'JNE': (this.flag !== 0b001);
+      
       case 'CMP': {
-        let flag = 0b000;
-        if (this.reg[regA] === this.reg[regB]) {
-          flag += 1
-        }
-        if (this.reg[regA] < this.reg[regB]) {
-          flag += 2
-        }
-        if (this.reg[regA] > this.reg[regB]) {
-          flag += 4
-        }
-        return flag;
+        if (this.reg[regA] === this.reg[regB]) { this.flag += 0b001; }
+        if (this.reg[regA] < this.reg[regB]) { this.flag += 0b010; }
+        if (this.reg[regA] > this.reg[regB]) { this.flag += 0b100; }
       }
-      case 'JEQ': if (this.flag === 1)
-      case 'JGT': if (this.flag === 2)
-      case 'JLT': if (this.flag === 4)
-      case 'JNE': if (this.flag === 0)
     }
   }
 
@@ -169,16 +145,13 @@ class CPU {
    */
   tick() {
     // Load the instruction register (IR) from the current PC
-    // !!! IMPLEMENT ME
     this.reg.IR = this.ram.read(this.reg.PC);
     let operandA = this.ram.read(this.reg.PC + 1);
     let operandB = this.ram.read(this.reg.PC + 2);
+
     // Debugging output
-    console.log(
-      `PC: ${this.reg.PC} RAMREAD: ${this.reg}
-      IR: ${this.reg.IR} ?MEM/PROP: ${operandA} ?MEM/PROP: ${operandB}
-      SP_ADDR: ${this.reg[SP_ADDR]}`
-    );
+    console.log(`PC: ${this.reg.PC} RAMREAD: ${this.reg} IR: ${this.reg.IR} ?MEM/PROP: ${operandA} ?MEM/PROP: ${operandB} SP_ADDR: ${this.reg[SP_ADDR]}`);
+
     // Based on the value in the Instruction Register, locate the
     // appropriate hander in the branchTable        
     let handler = this.branchTable[this.reg.IR];
@@ -195,22 +168,23 @@ class CPU {
       }
     }
   }
-
+  
+  // Debugging output
   // INSTRUCTION HANDLER CODE:
   // Gate Operations
-  NOT(regA) { this.alu('NOT', regA) }
+  NOT(regA)       { this.alu('NOT', regA) }
   AND(regA, regB) { this.alu('AND', regA, regB) }
-  OR(regA, regB) { this.alu('OR', regA, regB) }
+  OR(regA, regB)  { this.alu('OR', regA, regB) }
   XOR(regA, regB) { this.alu('XOR', regA, regB) }
   // Math Operations
-  INC(regA) { this.alu('INC', regA) }
-  DEC(regA) { this.alu('DEC', regA) }
+  INC(regA)       { this.alu('INC', regA) }
+  DEC(regA)       { this.alu('DEC', regA) }
   ADD(regA, regB) { this.alu('ADD', regA, regB) }
   SUB(regA, regB) { this.alu('SUB', regA, regB) }
   MUL(regA, regB) { this.alu('MUL', regA, regB) }
   DIV(regA, regB) { this.alu('DIV', regA, regB) }
   MOD(regA, regB) { this.alu('MOD', regA, regB) }
-  CMP(regA, regB) { this.flag = this.alu('CMP', regA, regB) }
+  CMP(regA, regB) { this.flag = 0b000; this.flag = this.alu('CMP', regA, regB); }
   // Core Operations
   NOP() { this.alu('NOP') }
   HLT() { this.stopClock() }
@@ -231,12 +205,11 @@ class CPU {
   POP(regNum) { this.stackHelper('POP', this.reg[regNum]) }
   RET() { this.stackHelper('RET') }
 
-  JMP(regA) {}
-  JEQ(regA) { this.alu('JEQ', regA) }
-  JGT(regA) { this.alu('JGT', regA) }
-  JLT(regA) { this.alu('JLT', regA) }
-  JNE(regA) { this.alu('JNE', regA) }
-
+  JMP(regNum) { this.reg.IR = regNum }
+  JEQ(regNum) { if (this.alu('JEQ')) this.reg.IR = regNum; }
+  JGT(regNum) { if (this.alu('JGT')) this.reg.IR = regNum; }
+  JLT(regNum) { if (this.alu('JLT')) this.reg.IR = regNum; }
+  JNE(regNum) { if (this.alu('JNE')) this.reg.IR = regNum; }
 }
 
 
