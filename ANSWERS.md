5598846dc3f336dfc03cd22f0577ba078dc480d4
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-13 12:31:44 -0400

    Completed ANSWERS

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,12 +1,16 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
-1. Explain how the CPU provides concurrency:
+## 1. Explain how the CPU provides concurrency:
+  By using a seperate 'process' or 'thread' to execute programs. Dedicated memory space is allocated for each process so they can maintain it's own 'state' e.g. 'Blocked', 'Ready', 'Running'. This allows the operating system to switch between different tasks and pickup where it left off.
+
+## 2. Describe assembly language and machine language:
+  Machine language is in binary and can be executed by the CPU. It is the lowest level of code used by programmers. Any languages that are higher level will typically get compiled down to down to machine code.
+
+  Assembly language is essentially machine code with a layer of abstraction to make it more human readable. It will typically use mnemonics to represent instructions, registers and other resources like memory and peripherals. Assembly code is not directly readable by the CPU. It needs to be converted with a compiler or assembler.
 
 
-2. Describe assembly language and machine language:
+## 3. Why is 3D performance so much higher with a graphics card than without?
+  The CPU is not needed to write graphics to the display and so it has a seperate channel dedicated to that, which in turn frees up the CPU to manage other resources. The GPU also has higher bandwidth to access memory.
 
-
-3. Why is 3D performance so much higher with a graphics card than without?
-
-
-4. Suggest the role that graphics cards play in machine learning:
+## 4. Suggest the role that graphics cards play in machine learning:
+  GPU's are built to do the same operation on different sets of data more efficiently and compute the same instructions in parallel. They also have a higher bandwidth to retrive from memory. The strengths of GPU's are ideal for executing the type of computations that machine learning employs.
--- ls8/cpu.js
+++ ls8/cpu.js
@@ -11,9 +11,15 @@
  */
 const ADD = 0b10101000;
 const CALL = 0b01001000;
+const CMP = 0b10100000;
+const DEC = 0b01111001;
+const DIV = 0b10101011;
 const HLT = 0b00000001;
+const INC = 0b01111000;
 const IRET = 0b00001011;
+const JEQ = 0b01010001;
 const JMP = 0b01010000;
+const JNE = 0b01010010;
 const LDI = 0b10011001;
 const MUL = 0b10101010;
 const POP = 0b01001100;
@@ -22,54 +28,49 @@ const PRN = 0b01000011;
 const PUSH = 0b01001101;
 const RET = 0b00001001;
 const ST = 0b10011010;
-const DEC = 0b01111001;
-const DIV = 0b10101011;
-const INC = 0b01111000;
 const SUB = 0b10101001;
-const CMP = 0b10100000;
 
 const SP = 7;
 const IS = 6;
 const IM = 5;
 
 class CPU {
+  /* Initialize the CPU */
+  constructor(ram) {
+    this.ram = ram;
+    this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
 
-    /* Initialize the CPU */
-    constructor(ram) {
-        this.ram = ram;
-        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
+    this.reg[ SP ] = this.ram.read(Number(0xf4)); // Stack Pointer (SP)
 
-        this.reg[SP] = this.ram.read(Number(0xf4)); // Stack Pointer (SP)
+    // Special-purpose registers
+    this.reg.PC = 0; // Program Counter
+    this.reg.FL = 0; // Flag
 
-        // Special-purpose registers
-        this.reg.PC = 0; // Program Counter
-        this.reg.FL = 0; // Flag
+    this.disableInterrupts = true;
+  }
 
-        this.disableInterrupts = true;
-    }
+  /* Store value in memory address, useful for program loading */
+  poke(address, value) {
+    this.ram.write(address, value);
+  }
 
-    /* Store value in memory address, useful for program loading */
-    poke(address, value) {
-        this.ram.write(address, value);
-    }
+  /* Starts the clock ticking on the CPU */
+  startClock() {
+    this.clock = setInterval(() => {
+      this.tick();
+    }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+    this.intClock = setInterval(() => {
+      this.intTick();
+    }, 1000); // Fires every 1 second (1000ms)
+  }
 
-    /* Starts the clock ticking on the CPU */
-    startClock() {
-        this.clock = setInterval(() => {
-            this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
-        this.intClock = setInterval(() => {
-            this.intTick();
-        }, 1000); // Fires every 1 second (1000ms)
-    }
+  /* Stops the clock */
+  stopClock() {
+    clearInterval(this.clock);
+    clearInterval(this.intClock);
+  }
 
-    /* Stops the clock */
-    stopClock() {
-        clearInterval(this.clock);
-        clearInterval(this.intClock);
-    }
-
-    /**
+  /**
    * ALU functionality
    *
    * The ALU is responsible for math and comparisons.
@@ -79,213 +80,222 @@ class CPU {
    *
    * op can be: ADD SUB MUL DIV INC DEC CMP
    */
-    alu(op, regA, regB) {
-        switch (op) {
-        case 'ADD':
-            this.reg[regA] = this.reg[regA] + this.reg[regB];
-            break;
-        case 'MUL':
-            this.reg[regA] = this.reg[regA] * this.reg[regB];
-            break;
-        case 'DEC':
-            this.reg[regA]--;
-            break;
-        case 'DIV':
-            if (this.reg[regB] === 0) {
-                console.log('Error: Value of Reg. B is 0!');
-                this.stopClock();
-            }
-            this.reg[regA] = this.reg[regA] / this.reg[regB];
-            break;
-        case 'INC':
-            this.reg[regA]++;
-            break;
-        case 'SUB':
-            this.reg[regA] = this.reg[regA] - this.reg[regB];
-            break;
-        case 'CMP':
-            if (this.reg[regA] < this.reg[regB]) {
-                this.reg.FL = 0b00000100;
-            } else if (this.reg[regA] > this.reg[regB]) {
-                this.reg.FL = 0b00000010;
-            } else if (this.reg[regA] === this.reg[regB]) {
-                this.reg.FL = 0b00000001;
-            }
-            break;
-        }
+  alu(op, regA, regB) {
+    switch (op) {
+    case 'ADD':
+      this.reg[ regA ] = this.reg[ regA ] + this.reg[ regB ];
+      break;
+    case 'MUL':
+      this.reg[ regA ] = this.reg[ regA ] * this.reg[ regB ];
+      break;
+    case 'DEC':
+      this.reg[ regA ]--;
+      break;
+    case 'DIV':
+      if (this.reg[ regB ] === 0) {
+        console.log('Error: Value of Reg. B is 0!');
+        this.stopClock();
+      }
+      this.reg[ regA ] = this.reg[ regA ] / this.reg[ regB ];
+      break;
+    case 'INC':
+      this.reg[ regA ]++;
+      break;
+    case 'SUB':
+      this.reg[ regA ] = this.reg[ regA ] - this.reg[ regB ];
+      break;
+    case 'CMP':
+      if (this.reg[ regA ] < this.reg[ regB ]) {
+        this.reg.FL = 0b00000100;
+      } else if (this.reg[ regA ] > this.reg[ regB ]) {
+        this.reg.FL = 0b00000010;
+      } else if (this.reg[ regA ] === this.reg[ regB ]) {
+        this.reg.FL = 0b00000001;
+      }
+      break;
     }
+  }
 
-    intTick() {
-        this.reg[IS] = 0b00000001;
-        let maskedInterrupts = this.reg[IM] & this.reg[IS];
+  intTick() {
+    this.reg[ IS ] = 0b00000001;
+    let maskedInterrupts = this.reg[ IM ] & this.reg[ IS ];
 
-        if (this.reg[IM]) {
-            for (let i = 0; i <= 7; i++) {
-                if ((maskedInterrupts >> i & 1) == 1) {
-                    this.reg[IS] = 0;
+    if (this.reg[ IM ]) {
+      for (let i = 0; i <= 7; i++) {
+        if (((maskedInterrupts >> i) & 1) == 1) {
+          this.reg[ IS ] = 0;
 
-                    // push the PC onto the stack
-                    this.reg[SP]--;
-                    this.ram.write(this.reg[SP], this.reg.PC);
+          // push the PC onto the stack
+          this.reg[ SP ]--;
+          this.ram.write(this.reg[ SP ], this.reg.PC);
 
-                    // push registers 0-6 onto the stack
-                    for (let i = 0; i <= 6; i++) {
-                        this.reg[SP]--;
-                        this.ram.write(this.reg[SP], this.reg[i]);
-                    }
-                    // Set the vector(address) of the interrupt handler to PC
-                    this.reg.PC = this.ram.read(0xf8);
+          // push registers 0-6 onto the stack
+          for (let i = 0; i <= 6; i++) {
+            this.reg[ SP ]--;
+            this.ram.write(this.reg[ SP ], this.reg[ i ]);
+          }
+          // Set the vector(address) of the interrupt handler to PC
+          this.reg.PC = this.ram.read(0xf8);
 
-                    break;
-                }
-            }
+          break;
         }
+      }
     }
+  }
 
-    /* Advances the CPU one cycle */
-    tick() {
+  /* Advances the CPU one cycle */
+  tick() {
     // Load the instruction register (IR--can just be a local variable here)
     // from the memory address pointed to by the PC. (I.e. the PC holds the
     // index into memory of the instruction that's about to be executed
     // right now.)
-        let IR = this.ram.read(this.reg.PC);
-        let callHandler;
-        let intHandler;
-        let jmpHandler;
+    let IR = this.ram.read(this.reg.PC);
+    let value;
 
-        // Debugging output
-        // console.log(`${this.reg.PC}: ${IR.toString(2)}`);
+    // Debugging output
+    // console.log(`${this.reg.PC}: ${IR.toString(2)}`);
 
-        // Get the two bytes in memory _after_ the PC in case the instruction
-        // needs them.
-        let operandA = this.ram.read(this.reg.PC + 1);
-        let operandB = this.ram.read(this.reg.PC + 2);
+    // Get the two bytes in memory _after_ the PC in case the instruction
+    // needs them.
+    let operandA = this.ram.read(this.reg.PC + 1);
+    let operandB = this.ram.read(this.reg.PC + 2);
 
-        // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
-        const handle_ADD = () => {
-            this.alu('ADD', operandA, operandB);
-        };
+    // Execute the instruction. Perform the actions for the instruction as
+    // outlined in the LS-8 spec.
+    const handle_ADD = () => {
+      this.alu('ADD', operandA, operandB);
+    };
 
-        const handle_CALL = () => {
-            this.reg[SP]--;
-            this.ram.write(this.reg[SP], this.reg.PC + (IR >>> 6) + 1);
-            callHandler = this.reg[operandA];
+    const handle_CALL = () => {
+      this.reg[ SP ]--;
+      this.ram.write(this.reg[ SP ], this.reg.PC + (IR >>> 6) + 1);
+      value = this.reg[ operandA ];
+      return value;
+    };
 
-            return callHandler;
-        };
+    const handle_DEC = () => {
+      this.alu('DEC', operandA);
+    };
 
-        const handle_DEC = () => {
-            this.alu('DEC', operandA);
-        };
+    const handle_DIV = () => {
+      this.alu('DIV', operandA, operandB);
+    };
 
-        const handle_DIV = () => {
-            this.alu('DIV', operandA, operandB);
-        };
+    const handle_HLT = () => {
+      this.stopClock();
+    };
 
-        const handle_HLT = () => {
-            this.stopClock();
-        };
+    const handle_INC = () => {
+      this.alu('INC', operandA);
+    };
 
-        const handle_INC = () => {
-            this.alu('INC', operandA);
-        };
+    const handle_IRET = () => {
+      for (let i = 6; i >= 0; i--) {
+        this.reg[ i ] = this.ram.read(this.reg[ SP ]);
+        this.reg[ SP ]++;
+      }
+      value = this.ram.read(this.reg[ SP ]);
+    };
 
-        const handle_IRET = () => {
-            for (let i = 6; i >= 0; i--) {
-                this.reg[i] = this.ram.read(this.reg[SP]);
-                this.reg[SP]++;
-            }
-            intHandler = this.ram.read(this.reg[SP]);
-        };
+    const handle_JEQ = () => {
+      if (this.reg.FL === 0b00000001) {
+        value = this.reg[ operandA ];
+        return value;
+      }
+    };
 
-        const handle_JMP = () => {
-            this.reg.PC = this.reg[operandA];
-            jmpHandler = this.reg[operandA];
-        };
+    const handle_JMP = () => {
+      value = this.reg[ operandA ];
+      return value;
+    };
 
-        const handle_LDI = () => {
-            this.reg[operandA] = operandB;
-        };
+    const handle_JNE = () => {
+      if (this.reg.FL === 0b00000000) {
+        value = this.reg[ operandA ];
+        return value;
+      }
+    };
 
-        const handle_MUL = () => {
-            this.alu('MUL', operandA, operandB);
-        };
+    const handle_LDI = () => {
+      this.reg[ operandA ] = operandB;
+    };
 
-        const handle_POP = () => {
-            this.reg[operandA] = this.ram.read(this.reg[SP]);
-            this.reg[SP]++;
-        };
+    const handle_MUL = () => {
+      this.alu('MUL', operandA, operandB);
+    };
 
-        const handle_PRA = () => {
-            console.log(String.fromCharCode(this.reg[operandA]));
-        };
+    const handle_POP = () => {
+      this.reg[ operandA ] = this.ram.read(this.reg[ SP ]);
+      this.reg[ SP ]++;
+    };
 
-        const handle_PRN = () => {
-            console.log(this.reg[operandA]);
-        };
+    const handle_PRA = () => {
+      console.log(String.fromCharCode(this.reg[ operandA ]));
+    };
 
-        const handle_PUSH = () => {
-            this.reg[SP]--;
-            this.ram.write(this.reg[SP], this.reg[operandA]);
-        };
+    const handle_PRN = () => {
+      console.log(this.reg[ operandA ]);
+    };
 
-        const handle_RET = () => {
-            callHandler = this.ram.read(this.reg[SP]);
-            this.reg[SP]++;
+    const handle_PUSH = () => {
+      this.reg[ SP ]--;
+      this.ram.write(this.reg[ SP ], this.reg[ operandA ]);
+    };
 
-            return callHandler;
-        };
+    const handle_RET = () => {
+      callHandler = this.ram.read(this.reg[ SP ]);
+      this.reg[ SP ]++;
 
-        const handle_ST = () => {
-            this.ram.write(this.reg[operandA], this.reg[operandB]);
-        };
+      return callHandler;
+    };
 
-        const handle_SUB = () => {
-            this.alu('SUB', operandA, operandB);
-        };
+    const handle_ST = () => {
+      this.ram.write(this.reg[ operandA ], this.reg[ operandB ]);
+    };
 
-        const handle_CMP = () => {
-            this.alu('CMP', operandA, operandB);
-        };
+    const handle_SUB = () => {
+      this.alu('SUB', operandA, operandB);
+    };
 
-        const branchTable = {
-            [ADD]: handle_ADD,
-            [CALL]: handle_CALL,
-            [DEC]: handle_DEC,
-            [DIV]: handle_DIV,
-            [HLT]: handle_HLT,
-            [INC]: handle_INC,
-            [IRET]: handle_IRET,
-            [JMP]: handle_JMP,
-            [LDI]: handle_LDI,
-            [MUL]: handle_MUL,
-            [POP]: handle_POP,
-            [PRA]: handle_PRA,
-            [PRN]: handle_PRN,
-            [PUSH]: handle_PUSH,
-            [RET]: handle_RET,
-            [ST]: handle_ST,
-            [SUB]: handle_SUB,
-            [CMP]: handle_CMP
-        };
+    const handle_CMP = () => {
+      this.alu('CMP', operandA, operandB);
+    };
 
-        branchTable[IR](operandA, operandB);
+    const branchTable = {
+      [ ADD ]: handle_ADD,
+      [ CALL ]: handle_CALL,
+      [ DEC ]: handle_DEC,
+      [ DIV ]: handle_DIV,
+      [ HLT ]: handle_HLT,
+      [ INC ]: handle_INC,
+      [ IRET ]: handle_IRET,
+      [ JEQ ]: handle_JEQ,
+      [ JMP ]: handle_JMP,
+      [ JNE ]: handle_JNE,
+      [ LDI ]: handle_LDI,
+      [ MUL ]: handle_MUL,
+      [ POP ]: handle_POP,
+      [ PRA ]: handle_PRA,
+      [ PRN ]: handle_PRN,
+      [ PUSH ]: handle_PUSH,
+      [ RET ]: handle_RET,
+      [ ST ]: handle_ST,
+      [ SUB ]: handle_SUB,
+      [ CMP ]: handle_CMP
+    };
 
-        // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
-        if (intHandler) {
-            this.reg.PC = intHandler;
-        } else if (callHandler) {
-            this.reg.PC = callHandler;
-        } else if (jmpHandler) {
-            this.reg.PC = jmpHandler;
-        } else {
-            this.reg.PC += (IR >>> 6) + 1;
-        }
+    branchTable[ IR ](operandA, operandB);
+
+    // Increment the PC register to go to the next instruction. Instructions
+    // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
+    // instruction byte tells you how many bytes follow the instruction byte
+    // for any particular instruction.
+    if (value) {
+      this.reg.PC = value;
+    } else {
+      this.reg.PC += (IR >>> 6) + 1;
     }
+  }
 }
 
 module.exports = CPU;
