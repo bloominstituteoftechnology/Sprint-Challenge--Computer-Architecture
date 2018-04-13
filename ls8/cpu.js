47f51176b78694d4386aff1ec39f75bd602d6259
Giraud Julemis <giraud.julemis@gmail.com>
2018-04-13 11:01:34 -0400

    added instructions

--- ls8/cpu.js
+++ ls8/cpu.js
@@ -1,7 +1,21 @@
 const HLT = 0b00000001;
 const LDI = 0b10011001;
 const MUL = 0b10101010;
+const ADD = 0b10101000;
+const SUB = 0b10101001;
+const DIV = 0b10101011;
+const MOD = 0b10101100;
+const INC = 0b01111000;
+const DEC = 0b01111001;
+const AND = 0b10110011;
+const NOT = 0b01110000;
+const OR = 0b10110001;
+const XOR = 0b10110010;
 const PRN = 0b01000011;
+const LD = 0b10011000;
+const NOP = 0b00000000;
+const PRA = 0b01000010;
+const ST = 0b10011010;
 const POP = 0b01001100;
 const PUSH = 0b01001101;
 const CALL = 0b10010000;
@@ -32,6 +46,44 @@ class CPU {
       case 'MUL':
         this.reg[regA] = this.reg[regA] * this.reg[regB];
         break;
+      case 'ADD':
+        this.reg[regA] = this.reg[regA] + this.reg[regB];
+        break;
+      case 'SUB':
+        this.reg[regA] = this.reg[regA] - this.reg[regB];
+        break;
+      case 'DIV':
+        if (this.reg[regB] === 0) {
+          console.error('Divider cannot be zero', this.reg[regB]);
+          this.stopClock();
+        }
+        this.reg[regA] = this.reg[regA] / this.reg[regB];
+        break;
+      case 'MOD':
+        if (this.reg[regB] === 0) {
+          console.error('Divider cannot be zero', this.reg[regB]);
+          this.stopClock();
+        }
+        this.reg[regA] = this.reg[regA] % this.reg[regB];
+        break;
+      case 'INC':
+        this.reg[regA] = (this.reg[regA] + 1) & 0xff;
+        break;
+      case 'DEC':
+        this.reg[regA] = (this.reg[regA] - 1) & 0xff;
+        break;
+      case 'AND':
+        this.reg[regA] = this.reg[regA] & this.reg[regB];
+        break;
+      case 'NOT':
+        this.reg[regA] = ~this.reg[regA];
+        break;
+      case 'OR':
+        this.reg[regA] = this.reg[regA] | this.reg[regB];
+        break;
+      case 'XOR':
+        this.reg[regA] = this.reg[regA] ^ this.reg[regB];
+        break;
     }
   }
   tick() {
@@ -44,11 +96,53 @@ class CPU {
       case LDI:
         this.reg[operandA] = operandB;
         break;
+      case LD:
+        this.reg[operandA] = this.ram.read(this.reg[operandB]);
+        break;
       case MUL:
         this.alu('MUL', operandA, operandB);
         break;
+      case DIV:
+        this.alu('DIV', operandA, operandB)
+        break;
+      case MOD:
+        this.alu('MOD', operandA, operandB);
+        break;
+      case ADD:
+        this.alu('ADD', operandA, operandB);
+        break;
+      case SUB:
+        this.alu('SUB', operandA, operandB);
+        break;
+      case INC:
+        this.alu('INC', operandA);
+        break;
+      case DEC:
+        this.alu('DEC', operandA);
+        break;
+      case AND:
+        this.alu('AND', operandA, operandB);
+        break;
+      case NOT:
+        this.alu('NOT', operandA);
+        break;
+      case OR:
+        this.alu('OR', operandA, operandB);
+        break;
+      case XOR:
+        this.alu('XOR', operandA, operandB);
+        break;
       case PRN:
         console.log(this.reg[operandA]);
+        break;
+      case NOP:
+        return undefined;
+        break;
+      case PRA:
+        console.log(String.fromCharCode(this.reg[operandA]));
+        break;
+      case ST:
+        this.ram.write(this.reg[operandA], this.reg[operandB]);
         break;
       case PUSH:
         this.reg[SP]--;
@@ -59,7 +153,7 @@ class CPU {
         this.reg[SP]++;
         break;
       case CALL:
-        
+
         break;
       case HLT:
         this.stopClock();
