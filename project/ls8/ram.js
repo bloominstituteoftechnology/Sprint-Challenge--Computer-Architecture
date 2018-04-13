3b9d1627c55013a8c6202c4b8290471fc0461376
Roy <Roy.tanjx@gmail.com>
2018-04-13 15:42:15 -0400

    2nd attempt to upload files properly

---
+++ project/ls8/ram.js
@@ -0,0 +1,29 @@
+/**
+ * RAM access
+ */
+class RAM {
+    constructor(size) {
+        this.mem = new Array(size);
+        this.mem.fill(0);
+    }
+
+    /**
+     * Write (store) MDR value at address MAR
+     */
+    write(MAR, MDR) {
+        // !!! IMPLEMENT ME
+        this.mem[MAR] = MDR;// write the value in the MDR to the address MAR
+    }
+
+    /**
+     * Read (load) MDR value from address MAR
+     * 
+     * @returns MDR
+     */
+    read(MAR) {
+        // !!! IMPLEMENT ME
+        return this.mem[MAR]// Read the value in address MAR and return it
+    }
+}
+
+module.exports = RAM;
