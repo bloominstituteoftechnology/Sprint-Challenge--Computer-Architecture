88c09d6e485982df6e3f58b34b1baee8a72daddc
JohnJohnX4 <JohnJohnX4@gmail.com>
2018-04-13 13:07:03 -0400

    Completed up to JMP

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -7,18 +7,21 @@ const CPU = require('./cpu');
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
-
-    // Hardcoded program to print the number 8 on the console
-
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
-
+    
+    const fileName = process.argv[2];
+    var fs = require('fs');
+    var program = fs
+      .readFileSync(fileName)
+      .toString()
+      .split('\n')
+      .reduce((array, line) => {
+        if (line[0] !== '#' && line[0] !== '') {
+          return array.concat(line.slice(0, 8));
+        }
+        return array;
+      }, [])
+      .filter(line => line !== '');
+  
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
         cpu.poke(i, parseInt(program[i], 2));
