9c72f684036a2f578d435aa715be20faabf5f855
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-06-15 10:44:11 -0700

    Clean up cpu.js file

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -21,68 +21,42 @@ const PUSH = 0b01001101;
 const CALL = 0b01001000;
 const RET = 0b00001001;
 const ST = 0b10011010;
+
 const CMP = 0b10100000;
 const JMP = 0b01010000;
 const JEQ = 0b01010001;
 const JNE = 0b01010010;
 
-/**
- * Class for simulating a simple Computer (CPU & memory)
- */
-
 const SP = 7;
 
 class CPU {
 
-    /**
-     * Initialize the CPU
-     */
     constructor(ram) {
         this.ram = ram;
 
-        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
+        this.reg = new Array(8).fill(0);
         
-        this.reg[SP] = 0xf4; // Stack Pointer Starting empty at Address F4
+        this.reg[SP] = 0xf4;
 
-        // Special-purpose registers
-        this.PC = 0; // Program Counter
+        this.PC = 0;
 
         this.FL = 0b00000000;
     }
     
-    /**
-     * Store value in memory address, useful for program loading
-     */
     poke(address, value) {
         this.ram.write(address, value);
     }
 
-    /**
-     * Starts the clock ticking on the CPU
-     */
     startClock() {
         this.clock = setInterval(() => {
             this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+        }, 1);
     }
 
-    /**
-     * Stops the clock
-     */
     stopClock() {
         clearInterval(this.clock);
     }
 
-    /**
-     * ALU functionality
-     *
-     * The ALU is responsible for math and comparisons.
-     *
-     * If you have an instruction that does math, i.e. MUL, the CPU would hand
-     * it off to it's internal ALU component to do the actual work.
-     *
-     * op can be: ADD SUB MUL DIV INC DEC CMP
-     */
     alu(op, regA, regB) {
         switch (op) {
             case 'ADD':
@@ -91,7 +65,6 @@ class CPU {
 
             case 'MUL':
                 this.reg[regA] *= this.reg[regB];
-                // !!! IMPLEMENT ME
                 break;
 
             case 'SUB':
@@ -124,9 +97,6 @@ class CPU {
         }
     }
 
-    /**
-     * Advances the CPU one cycle
-     */
     tick() {
         const push_it = x => {
             this.alu('DEC', SP);
@@ -139,29 +109,10 @@ class CPU {
             return popped;
         };
 
-        // Load the instruction register (IR--can just be a local variable here)
-        // from the memory address pointed to by the PC. (I.e. the PC holds the
-        // index into memory of the instruction that's about to be executed
-        // right now.)
-
         const IR = this.ram.read(this.PC);
-
-        // !!! IMPLEMENT ME
-
-        // Debugging output
-
-        // console.log(`${this.PC}: ${IR.toString(2)}`);
-
-        // Get the two bytes in memory _after_ the PC in case the instruction
-        // needs them.
 
         const operandA = this.ram.read(this.PC + 1);
         const operandB = this.ram.read(this.PC + 2);
-
-        // !!! IMPLEMENT ME
-
-        // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
 
         switch(IR) {
             case ADD:
@@ -201,7 +152,6 @@ class CPU {
                 break;
 
             case JMP:
-                // this isn't going to work since we increment PC after the switch statement
                 this.PC = this.reg[operandA];
                 break;
 
@@ -232,7 +182,6 @@ class CPU {
                 if (this.reg[operandA] < this.reg[operandB]) this.FL = 0b00000100;
                 if (this.reg[operandA] > this.reg[operandB]) this.FL = 0b00000010;
                 if (this.reg[operandA] === this.reg[operandB]) this.FL = 0b00000001;
-                // else this.FL = 0b00000000;
                 break;
 
             case JMP:
@@ -256,9 +205,7 @@ class CPU {
                 break;
 
             case LDI:
-                // Set the value in a register
                 this.reg[operandA] = operandB;
-                // this.PC += 3; // Next Instruction
                 // console.log(this.regs[operandA]);
                 break;
 
@@ -277,12 +224,6 @@ class CPU {
                 this.stopClock();
                 return;
         }
-        // !!! IMPLEMENT ME
-
-        // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
 
         if (IR !== CALL && IR !== JMP && IR !== RET && IR !== JEQ && IR !== JNE) {
             this.PC += 1 + (IR >> 6);
@@ -291,7 +232,6 @@ class CPU {
         // this.PC += instLen;
 
         // console.log(InstLen);
-        // !!! IMPLEMENT ME
     }
 }
 
