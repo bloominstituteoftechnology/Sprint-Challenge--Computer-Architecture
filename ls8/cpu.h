437d761541eb9fd2dceacd2587ec1255a7098a1d
Katie Gorbell <katiegorbell@gmail.com>
2018-10-19 15:30:05 -0500

    ls8 file removed from gitignore

---
+++ ls8/cpu.h
@@ -0,0 +1,53 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+
+// Holds all information about the CPU
+struct cpu {
+  // TODO
+  // PC
+  unsigned char pc;
+  unsigned char ir;
+  unsigned char fl;
+  // registers (array)
+  unsigned char registers[8];
+   // ram (array)
+  unsigned char ram[256];
+};
+
+// ALU operations
+enum alu_op {
+	ALU_MUL,
+  ALU_ADD,
+  ALU_CMP
+	// Add more here
+};
+
+// Instructions
+
+// These use binary literals. If these aren't available with your compiler, hex
+// literals should be used.
+
+#define LDI  0b10000010 
+#define PRN  0b01000111
+#define HLT  0b00000001
+#define MUL  0b10100010
+#define PUSH 0b01000101
+#define POP 0b01000110
+#define JMP 0b01010100
+#define CALL 0b01010000
+#define RET 0b00010001
+#define ADD 0b10100000
+#define JEQ 0b01010001
+#define JNE 0b01010010
+#define CMP 0b10100111
+
+// TODO: more instructions here. These can be used in cpu_run().
+
+// Function declarations
+
+extern void cpu_load(struct cpu *cpu, char *file);
+extern void cpu_init(struct cpu *cpu);
+extern void cpu_run(struct cpu *cpu);
+
+#endif
+
