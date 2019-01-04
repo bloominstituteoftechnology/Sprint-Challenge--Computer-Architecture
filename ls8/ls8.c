728acb6210f094f5c570654b5f3a20c240c6cee3
JohnCoronel <john.m.coronel@gmail.com>
2019-01-04 08:30:20 -0800

    working ls8 with cmp,jmp jne jeq

---
+++ ls8/ls8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "cpu.h"
+
+/**
+ * Main
+ */
+int main(int argc, char **argv)
+{
+  struct cpu cpu;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: ls8 file.ls8\n");
+    return 1;
+  }
+
+  cpu_init(&cpu);
+  cpu_load(argv[1], &cpu);
+  cpu_run(&cpu);
+
+  return 0;
+}
