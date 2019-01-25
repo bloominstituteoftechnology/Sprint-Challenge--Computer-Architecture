a98609b323a2a7d450732f0058d501dce963450b
travis-russell <russell.travis@yahoo.com>
2019-01-25 10:49:12 -0600

    defined

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -29,6 +29,10 @@ enum alu_op
 #define PRN 0b01000111
 #define PUSH 0b01000101
 #define RET 0b00010001
+#define CMP 0b10100111
+#define JEQ 0b01010101
+#define JMP 0b01010100
+#define JNE 0b01010110
 
 extern void cpu_load(char *filename, struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
