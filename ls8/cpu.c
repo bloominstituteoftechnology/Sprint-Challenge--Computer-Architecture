0ba0f8b786a8bd1a961d5f3a4788eaeb70866ef2
James Cassidy <cassidyjamesw@gmail.com>
2019-01-25 12:32:35 -0500

    copied

---
+++ ls8/cpu.c
@@ -0,0 +1,127 @@
+#include "cpu.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DATA_LEN 6
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+  return cpu->ram[address];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+{
+  cpu->ram[address] = value;
+}
+
+/**
+ * Load the binary bytes from a .ls8 source file into a RAM array
+ */
+void cpu_load(struct cpu *cpu, char *filename)
+{
+  FILE *fptr;
+  char line[1024];
+  unsigned char address = 0;
+  fptr = fopen(filename, "r");
+
+  if (fptr == NULL)
+  {
+    fprintf(stderr, "Error opening file");
+    exit(1);
+  }
+
+  while (fgets(line, sizeof line, fptr) != NULL)
+  {
+
+    unsigned char b;
+    b = strtoul(line, NULL, 2);
+    cpu_ram_write(cpu, address++, b);
+  }
+  fclose(fptr);
+}
+
+/**
+ * ALU
+ */
+void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+{
+  switch (op)
+  {
+  case ALU_MUL:
+    cpu->registers[regA] *= cpu->registers[regB];
+    break;
+  case ALU_ADD:
+    cpu->registers[regA] += cpu->registers[regB];
+    break;
+  }
+}
+
+/**
+ * Run the CPU
+ */
+void cpu_run(struct cpu *cpu)
+{
+  int running = 1;
+
+  while (running)
+  {
+
+    unsigned char IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned char operandA = cpu_ram_read(cpu, (cpu->PC + 1));
+    unsigned char operandB = cpu_ram_read(cpu, (cpu->PC + 2));
+
+    int add_to_pc = (IR >> 6) + 1;
+
+    switch (IR)
+    {
+    case LDI:
+      cpu->registers[operandA] = operandB;
+      break;
+    case PRN:
+      printf("%d \n", cpu->registers[operandA]);
+      break;
+    case HLT:
+      running = 0;
+      break;
+    case MUL:
+      alu(cpu, ALU_MUL, operandA, operandB);
+      break;
+    case PUSH:
+      cpu->registers[7] -= 1;
+      cpu->ram[cpu->registers[7]] = cpu->registers[operandA];
+      break;
+    case POP:
+      cpu->registers[operandA] = cpu->ram[cpu->registers[7]];
+      cpu->registers[7]++;
+      break;
+    case ADD:
+      alu(cpu, ALU_ADD, operandA, operandB);
+      break;
+    case CALL:
+      cpu->registers[7] -= 1;
+      cpu->ram[cpu->registers[7]] = cpu->PC + 2;
+      cpu->PC = cpu->registers[operandA];
+      add_to_pc = 0;
+      break;
+    case RET:
+      cpu->PC = cpu->ram[cpu->registers[7]];
+      cpu->registers[7] += 1;
+      add_to_pc = 0;
+      break;
+    }
+
+    cpu->PC += add_to_pc;
+  }
+}
+
+/**
+ * Initialize a CPU struct
+ */
+void cpu_init(struct cpu *cpu)
+{
+  cpu->PC = 0;
+
+  memset(cpu->ram, 0, sizeof cpu->ram);
+  memset(cpu->registers, 0, sizeof cpu->registers);
+}
---
+++ ls8/cpu.h
@@ -0,0 +1,37 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+
+// Holds all information about the CPU
+struct cpu
+{
+  unsigned char PC;           // PC
+  unsigned char registers[8]; // registers (array)
+  unsigned char ram[256];     // ram (array)
+};
+
+// ALU operations
+enum alu_op
+{
+  ALU_MUL,
+  ALU_ADD,
+  // Add more here
+};
+
+#define LDI 0b10000010
+#define PRN 0b01000111
+#define HLT 0b00000001
+#define MUL 0b10100010
+#define PUSH 0b01000101
+#define POP 0b01000110
+#define ADD 0b10100000
+#define RET 0b00010001
+#define CALL 0b01010000
+// TODO: more instructions here. These can be used in cpu_run().
+
+// Function declarations
+
+extern void cpu_load(struct cpu *cpu, char *filename);
+extern void cpu_init(struct cpu *cpu);
+extern void cpu_run(struct cpu *cpu);
+
+#endif
