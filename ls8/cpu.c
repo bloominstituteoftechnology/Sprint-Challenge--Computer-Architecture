cfc58a6175567173dfb042da16575869501921b6
James Cassidy <cassidyjamesw@gmail.com>
2019-01-25 13:19:41 -0500

    some refactoring

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,9 +1,9 @@
-#include "cpu.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "cpu.h"
 
-#define DATA_LEN 6
+#define SP 7
 
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
 {
@@ -15,147 +15,151 @@ void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
   cpu->ram[address] = value;
 }
 
-/**
- * Load the binary bytes from a .ls8 source file into a RAM array
- */
-void cpu_load(struct cpu *cpu, char *filename)
+void cpu_load(struct cpu *cpu, char *argv[])
 {
-  FILE *fptr;
-  char line[1024];
+  FILE *fp;
+  char data[1024];
   unsigned char address = 0;
-  fptr = fopen(filename, "r");
+  fp = fopen(argv[1], "r");
 
-  if (fptr == NULL)
+  if (!fp)
   {
-    fprintf(stderr, "Error opening file");
+    printf("Error opening file \n");
     exit(1);
   }
 
-  while (fgets(line, sizeof line, fptr) != NULL)
+  while (fgets(data, sizeof(data), fp) != NULL)
   {
-
-    unsigned char b;
-    b = strtoul(line, NULL, 2);
-    cpu_ram_write(cpu, address++, b);
+    char *endptr;
+    unsigned char ret = strtol(data, &endptr, 2);
+    if (endptr == data)
+    {
+      continue;
+    }
+    cpu->ram[address++] = ret;
   }
-  fclose(fptr);
+
+  fclose(fp);
 }
 
-/**
- * ALU
- */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
   switch (op)
   {
   case ALU_MUL:
-    cpu->registers[regA] *= cpu->registers[regB];
+    cpu->reg[regA] *= cpu->reg[regB];
     break;
   case ALU_ADD:
-    cpu->registers[regA] += cpu->registers[regB];
+    cpu->reg[regA] += cpu->reg[regB];
     break;
   case ALU_CMP:
-    if (cpu->registers[regA] > cpu->registers[regB])
+    if (cpu->reg[regA] == cpu->reg[regB])
     {
-      cpu->flag = 00000010;
+      cpu->FL = cpu->FL | (1 << 0);
     }
-    else if (cpu->registers[regA] < cpu->registers[regB])
+    else if (cpu->reg[regA] > cpu->reg[regB])
     {
-      cpu->flag = 00000100;
+      cpu->FL = cpu->FL | (1 << 1);
     }
     else
     {
-      cpu->flag = 00000001;
+      cpu->FL = cpu->FL | (1 << 2);
     }
+    break;
+
+  default:
+    break;
   }
 }
 
-/**
- * Run the CPU
- */
+unsigned char cpu_pop(struct cpu *cpu)
+{
+  unsigned char ret = cpu->ram[cpu->reg[SP]];
+  cpu->reg[SP]++;
+  return ret;
+}
+
+void cpu_push(struct cpu *cpu, unsigned char val)
+{
+  cpu->reg[SP]--;
+  cpu->ram[cpu->reg[SP]] = val;
+}
+
 void cpu_run(struct cpu *cpu)
 {
   int running = 1;
 
   while (running)
   {
-
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);
-    unsigned char operandA = cpu_ram_read(cpu, (cpu->PC + 1));
-    unsigned char operandB = cpu_ram_read(cpu, (cpu->PC + 2));
-
-    int add_to_pc = (IR >> 6) + 1;
+    unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
+    unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
+    int num_operands = (IR >> 6) + 1;
 
     switch (IR)
     {
     case LDI:
-      cpu->registers[operandA] = operandB;
+      cpu->reg[operandA] = operandB;
       break;
     case PRN:
-      printf("%d \n", cpu->registers[operandA]);
-      break;
-    case HLT:
-      running = 0;
+      printf("%d \n", cpu->reg[operandA]);
       break;
     case MUL:
       alu(cpu, ALU_MUL, operandA, operandB);
       break;
-    case PUSH:
-      cpu->registers[7] -= 1;
-      cpu->ram[cpu->registers[7]] = cpu->registers[operandA];
-      break;
-    case POP:
-      cpu->registers[operandA] = cpu->ram[cpu->registers[7]];
-      cpu->registers[7]++;
-      break;
     case ADD:
       alu(cpu, ALU_ADD, operandA, operandB);
       break;
+    case PUSH:
+      cpu_push(cpu, cpu->reg[operandA]);
+      break;
+    case POP:
+      cpu->reg[operandA] = cpu_pop(cpu);
+      break;
     case CALL:
-      cpu->registers[7] -= 1;
-      cpu->ram[cpu->registers[7]] = cpu->PC + 2;
-      cpu->PC = cpu->registers[operandA];
-      add_to_pc = 0;
+      cpu_push(cpu, cpu->PC + 2);
+      cpu->PC = cpu->reg[operandA] - 1;
+      num_operands = 0;
       break;
     case RET:
-      cpu->PC = cpu->ram[cpu->registers[7]];
-      cpu->registers[7] += 1;
-      add_to_pc = 0;
+      cpu->PC = cpu_pop(cpu);
+      num_operands = 0;
       break;
-    case CMP: //opcode to compare two registers
+    case HLT:
+      running = 0;
+      break;
+    case CMP:
       alu(cpu, ALU_CMP, operandA, operandB);
       break;
-    case JMP: // opcode to jump to the address stored at the given register
-      cpu->PC = cpu->registers[operandA];
-      add_to_pc = 0;
+    case JMP:
+      cpu->PC = cpu->reg[operandA];
+      num_operands = 0;
       break;
     case JEQ: // opcode to jump if the  equal flag is set to true
-      if (cpu->flag == 00000001)
+      if (cpu->FL == 00000001)
       {
-        cpu->PC = cpu->registers[operandA];
-        add_to_pc = 0;
+        cpu->PC = cpu->reg[operandA];
+        num_operands = 0;
       }
       break;
     case JNE:
-      if (cpu->flag != 00000001)
+      if (cpu->FL != 00000001)
       {
-        cpu->PC = cpu->registers[operandA];
-        add_to_pc = 0;
+        cpu->PC = cpu->reg[operandA];
+        num_operands = 0;
       }
       break;
     }
 
-    cpu->PC += add_to_pc;
+    cpu->PC += num_operands;
   }
 }
 
-/**
- * Initialize a CPU struct
- */
 void cpu_init(struct cpu *cpu)
 {
   cpu->PC = 0;
-
+  cpu->FL = 0x00;
+  cpu->reg[SP] = 0xF4;
   memset(cpu->ram, 0, sizeof cpu->ram);
-  memset(cpu->registers, 0, sizeof cpu->registers);
+  memset(cpu->reg, 0, sizeof cpu->reg);
 }
--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "cpu.h"
 
-/**
- * Main
- */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+  if (argc != 2)
+  {
+    printf("usage: ./ls8 filename\n");
+    return 1;
+  }
   struct cpu cpu;
 
   cpu_init(&cpu);
-  cpu_load(&cpu, argv[1]);
+  cpu_load(&cpu, argv);
   cpu_run(&cpu);
 
   return 0;
