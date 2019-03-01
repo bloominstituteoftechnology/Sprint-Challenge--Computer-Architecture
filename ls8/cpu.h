2acf51ac387bc7d56fdc8e7ca2e52a4563712601
Eva <evalee904@gmail.com>
2019-03-01 12:13:16 -0600

    added rest of the defines

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -26,6 +26,9 @@ enum alu_op
 #define CALL 0b01010000
 #define RET 0b00010001
 #define CMP 0b10100111
+#define JMP 0b01010100
+#define JNE 0b01010110
+#define JEQ 0b01010101
 
 extern void cpu_load(struct cpu *cpu, char *argv[]);
 extern void cpu_init(struct cpu *cpu);
