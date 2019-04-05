33ec56ce8153d2991628ac289455bd194ba00cb4
Carlos Lantigua <clantigua954@gmail.com>
2019-04-05 09:48:46 -0400

    initialized header file

---
+++ cpu.h
@@ -0,0 +1,50 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+
+// setup basic cpu struct
+struct CPU
+{
+    unsigned int PC;
+    // R7 is reserved as the stack pointer (SP)
+    unsigned int SP;
+    // 8 general-purpose 8-bit numeric registers R0 - R7
+    unsigned char reg[8];
+    /*These registers only hold values between 0-255.
+    After performing math on registers in the emulator,
+    bitwise-AND the result with 0xFF (255) to keep the
+    register values in that range.*/
+    unsigned char ram[256];
+};
+
+// ALU ops
+enum alu_op
+{
+    // add alu ops if needed
+};
+
+// setup instructions
+#define LDI 0b10000010
+#define PRN 0b01000111
+#define HLT 0b00000001
+#define PUSH 0b01000101
+//POP register - pop the value at the top of the stack into the given reg.
+#define POP 0b01000110
+// look into these they are new for the sprint
+#define CMP 0b10100111 // handled by ALU Compare the valu
+#define JEQ 0b01010101
+#define JNE 0b01010110
+// A lot of these commented out instructions might not be needed
+// #define MUL 0b10100010
+// #define CALL 0b01010000
+// #define RET 0b00010001
+// #define ADD 0b10100000
+
+// function declarations
+// load the instructions for the cpu
+extern void cpu_load(struct cpu *cpu, char *filename);
+// initialize the cpu (clear out all memory)
+extern void cpu_init(struct cpu *cpu);
+// run the instructions for the cpu
+extern void cpu_run(struct cpu *cpu);
+
+#endif
