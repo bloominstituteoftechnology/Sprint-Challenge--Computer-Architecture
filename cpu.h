8ac88c8e9fd457d818837372e441f511dcceb3c8
Carlos Lantigua <clantigua954@gmail.com>
2019-04-05 13:44:38 -0400

    got rid of some unrequired stuff

--- cpu.h
+++ cpu.h
@@ -8,8 +8,6 @@ struct cpu
     unsigned int PC;
     // FL = flag
     unsigned int FL;
-    // R7 is reserved as the stack pointer (SP)
-    unsigned int SP;
     // 8 general-purpose 8-bit numeric registers R0 - R7
     unsigned char reg[8];
     /*These registers only hold values between 0-255.
@@ -29,19 +27,12 @@ enum alu_op
 #define LDI 0b10000010
 #define PRN 0b01000111
 #define HLT 0b00000001
-// #define MUL 0b10100010
-// #define ADD 0b10100000
+
 #define JMP 0b01010100
-//POP register - pop the value at the top of the stack into the given reg.
-// #define PUSH 0b01000101
-// #define POP 0b01000110
 // look into these they are new for the sprint
 #define CMP 0b10100111 // Compare the value of regs
 #define JEQ 0b01010101
 #define JNE 0b01010110
-// A lot of these commented out instructions might not be needed
-// #define CALL 0b01010000
-// #define RET 0b00010001
 
 // function declarations
 // load the instructions for the cpu
