06ccf6d26694e4fe994003adc8bd46fdafb63ed6
Bao Pham <ryanvuepham@gmail.com>
2019-11-10 13:25:06 -0600

    added JEQ, JNE methods

--- cpu.py
+++ cpu.py
@@ -83,9 +83,17 @@ class CPU:
     def jmp(self, operand_a):
 		self.pc = self.reg[operand_a]
 
-    def jeq(self):
+    def jeq(self, operand_a):
+		if self.fl == 0b00000010:
+			self.pc = self.reg[operand_a]
+		else:
+			self.pc += 2
 
-    def jne(self):
+    def jne(self, operand_a):
+		if self != 0b00000010:
+			self.pc = self.reg[operand_a]
+		else:
+			self.pc += 2
 
 
     def load(self):
