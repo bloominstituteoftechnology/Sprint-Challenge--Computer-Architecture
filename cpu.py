88db0e292df3c205cdc4f3a6a0a77cc1c2f479e4
SarmenSinanian <sarmen.sinanian@yahoo.com>
2020-05-28 12:05:11 -0700

    MVP REACHED

--- cpu.py
+++ cpu.py
@@ -1,5 +1,4 @@
 """CPU functionality."""
-
 import sys
 
 
@@ -8,116 +7,102 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        # 8 slot register being created
-        self.register = {d: 0 for d in range(0, 8)}
-        # PC pointer created; start at 0
+        self.register = {k: 0 for k in range(0, 8)}
+        self.register[7] = 0xF4
         self.pc = 0
-        # 8 bit ram array created
+        self.fl = 0b00000000  # 00000LGE
         self.ram = [0] * 256
-
-        self.alu_operation = {
-            0b0111:self.CMP,
+        self.alu = {
+            0b0111: self.CMP,
+        }
+        self.pc_mutators = {
+            0b0000: self.CALL, # Call
+            0b0001: self.RET,  # Return
+            0b0100: self.JMP,  # Jump Register
+            0b0101: self.JEQ,  # Jump IF EQUAL
+            0b0110: self.JNE,  # Jump IF NOT EQUAL
+        }
+        self.general = {
+            0b0001: self.HLT,  # Halt
+            0b0010: self.LDI,  # Load Register Immediate
+            0b0011: self.LD,   # Load registerA with the value at mem address registerB
+            0b0100: self.ST,   # Store value in regB in address stored in regA
+            0b0101: self.PUSH, # Push value in given reg on to the stack
+            0b0110: self.POP,  # Pop value at top of stack INTO given register
+            0b0111: self.PRN,  # Print numeric in given register
         }
 
-        self.pc_modifier = {
-            0b0100: self.JMP,
-            0b0101: self.JEQ,
-            0b0110: self.JNE,
-        }
+    def load(self, filename=None):
 
-    def load(self):
         """Load a program into memory."""
-        # Starting address
         address = 0
-
-        # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-        # Loading instructions into ram
-        for instruction in program:
-            self.ram[address] = instruction
-            # print(f'{instruction} LOAD INSTRUCTION')
+        if len(sys.argv) > 1:
+            filename = sys.argv[1]
+        program = open(filename)
+        for line in program:
+            string_val = line.split("#")[0].strip()
+            if string_val == '':
+                continue
+            v = int(string_val, 2)
+            self.ram[address] = v
             address += 1
-
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-        #
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
 
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            self.fl,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
-
         for i in range(8):
             print(" %02X" % self.register[i], end='')
-
         print()
 
     def ram_read(self, position):
+        self.pc += 1
         return self.ram[position]
 
     def ram_write(self, position, value):
         self.ram[position] = value
 
-    def int_to_bin(self, instruction):
-        # print(f'{instruction} INSTRUCTION INT')
-        # Converting int to binary, chopping off 0b, and adding leading zeros to make instruction 8 bits long
-        instruction = bin(instruction)[2:].zfill(8)
-        # print(f'{instruction} INSTRUCTION BIN')
-        if instruction == "00000001":
-            return None
-        else:
-            # Converting outputs to binary/base 2
-            output = int(instruction[4:], 2)
-            return output
-
     def run(self):
         """Run the CPU."""
-        halted = False
-
-        while not halted:
+        self.halted = False
+        self.interrupted = False
+        while not self.halted:
             current_instruct = self.ram_read(self.pc)
-            binary = self.int_to_bin(current_instruct)
-            self.pc += 1
-            if binary is None:
-                halted = True
+            decoded = self.decode(current_instruct)
+            # If instruction uses the ALU
+            if decoded['is_alu']:
+                func = self.alu[decoded['id']]
+            # If instruction sets the PC
+            elif decoded['sets_pc']:
+                func = self.pc_mutators[decoded['id']]
+            # If neither of the above
             else:
-                if binary == 0b0010:  # LDI
-                    register_location = self.ram_read(self.pc)
-                    self.pc += 1
-                    register_value = self.ram_read(self.pc)
-                    self.pc += 1
-                    self.register[register_location] = register_value
-                elif binary == 0b0111:  # PRN
-                    register_location = self.ram_read(self.pc)
-                    self.pc += 1
-                    print(self.register[register_location])
+                func = self.general[decoded['id']]
+            if func is not None:
+                # Finally, call the func if given
+                if decoded['num_operands'] == 0:
+                    func()
+                elif decoded['num_operands'] == 1:
+                    reg_a = self.ram_read(self.pc)
+                    func(reg_a)
+                elif decoded['num_operands'] == 2:
+                    reg_a = self.ram_read(self.pc)
+                    reg_b = self.ram_read(self.pc)
+                    func(reg_a, reg_b)
+        # sys.exit(0)
 
-    def CMP(self, reg_a, reg_b):
+    # ALU
+
+    def CMP(self, reg_a, reg_b): # Compare
         val_a = self.register[reg_a]
         val_b = self.register[reg_b]
-
         if val_a == val_b:
             self.fl = 0b00000001
         elif val_a > val_b:
@@ -127,15 +112,57 @@ class CPU:
         else:
             self.fl = 0b0
 
-    def JMP(self, reg_a):
+    # PC Control
+    def CALL(self, reg): # Call
+        self.register[7] -= 1
+        self.ram[self.register[7]] = self.pc
+        self.pc = self.register[reg]
+
+    def RET(self): # Return
+        self.pc = self.ram[self.register[7]]
+        self.register[7] += 1
+
+    def JMP(self, reg_a): # Jump to address in reg
         self.pc = self.register[reg_a]
 
-    def JEQ(self, reg_a):
+    def JEQ(self, reg_a): # Jump to address in reg IF EQUAL
         a = bool(self.fl & 0b00000001)
         if a:
             self.pc = self.register[reg_a]
 
-    def JNE(self, reg_a):
+    def JNE(self, reg_a): # Jump to address in reg IF NOT EQUAL
         a = bool(self.fl & 0b00000001)
         if not a:
             self.pc = self.register[reg_a]
+
+    # General commands
+
+    def HLT(self): # Halt
+        self.halted = True
+
+    def LDI(self, reg_a, val): # Load Register Immediate
+        self.register[reg_a] = val
+
+    def LD(self, reg_a, reg_b): # Load registerA with the value at mem address registerB
+        self.register[reg_a] = self.ram[self.register[reg_b]]
+
+    def ST(self, reg_a, reg_b): # Store value in regB in address stored in regA
+        self.ram[self.register[reg_a]] = self.register[reg_b]
+
+    def PUSH(self, reg_a): # Push value in given reg on to the stack
+        self.register[7] -= 1
+        self.ram[self.register[7]] = self.register[reg_a]
+
+    def POP(self, reg_a): # Pop value at top of stack INTO given register
+        self.register[reg_a] = self.ram[self.register[7]]
+        self.register[7] += 1
+
+    def PRN(self, reg_a): # Print numeric in given register
+        value = self.register[reg_a]
+        print(value)
+
+    def decode(self, instruction): # Convert INT to BINARY; Create dictionary of outputs split by category
+        instruction = bin(instruction)[2:].zfill(8)
+        output = {'num_operands': int(instruction[:2], 2), 'is_alu': bool(int(instruction[2], 2)),
+                  'sets_pc': bool(int(instruction[3], 2)), 'id': int(instruction[4:], 2)}
+        return output
