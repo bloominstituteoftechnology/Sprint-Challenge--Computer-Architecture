1d0647524ab8780bc0946d64f8ad714ad1f92c1f
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-08-22 19:34:18 -0700

    Create cpu.py
    basic cpu boiler

---
+++ cpu.py
@@ -0,0 +1,21 @@
+import sys
+
+class CPU:
+	def __init__(self):
+		pass
+# set up RAM read
+	def ram_read():
+		pass
+# set up RAM write
+	def ram_write():
+		pass
+
+# def Load
+	def load(self):
+		""" Load a program into memory. """
+		pass
+
+# def running
+	while running:
+		pass
+
