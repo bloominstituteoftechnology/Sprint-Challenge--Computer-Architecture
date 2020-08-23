8721b04ca19559f10d9eea2d558ae07f43f379d6
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-08-22 20:44:19 -0700

    Update cpu.py
    defs for instructions and more runnning

--- cpu.py
+++ cpu.py
@@ -6,15 +6,15 @@ LDI = 0b10000010
 PRN = 0b01000111
 
 class CPU:
-	def __init__(self):+
+	def __init__(self):
 		## set to run
-        self.running = true
-        # set a counter
-        self.pc = 0
-        # set memory
-        self.ram = [0] * 256
-        # set registers
-        self.reg = [0] * 8
+		self.running = True
+		# set a counter
+		self.pc = 0
+		# set memory
+		self.ram = [0] * 256
+		# set registers
+		self.reg = [0] * 8
 		# set flag
 		self.flag = None
 # set up RAM read
@@ -24,12 +24,31 @@ class CPU:
 	def ram_write(self, value, address):
 		self.ram[address] = value
 
+# set up instructions ()
+
+	def HTL(self):
+		self.running = False
+
+	def LDI(self, reg_index, num_to_save):
+		num_to_save = self.ram[self.pc + 2]
+		reg_index = self.ram[self.pc + 1]
+		self.reg[reg_index] = num_to_save
+		self.pc += 3
+	
+	def PRN(self, reg_index, val_to_print):
+		val_to_print = self.ram[self.pc + 1]
+		self.pc += 2
+
+
+
 # def Load
 	def load(self):
 		""" Load a program into memory. """
 		pass
 
 # def running
-	while running:
-		pass
+	def run(self):
+		self.load()
+		while self.running:
+			pass
 
