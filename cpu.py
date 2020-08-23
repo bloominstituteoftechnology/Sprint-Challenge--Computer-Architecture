d7a1c26ca3d1bd0bbfe4c565bfe21e33d1cb6764
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-08-22 19:45:25 -0700

    Update cpu.py
    set up base operations and CPU

--- cpu.py
+++ cpu.py
@@ -1,8 +1,22 @@
 import sys
 
+## operations
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+
 class CPU:
-	def __init__(self):
-		pass
+	def __init__(self):+
+		## set to run
+        self.running = true
+        # set a counter
+        self.pc = 0
+        # set memory
+        self.ram = [0] * 256
+        # set registers
+        self.reg = [0] * 8
+		# set flag
+		self.flag = None
 # set up RAM read
 	def ram_read():
 		pass
