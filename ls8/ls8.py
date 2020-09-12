2bd900c6a05de22b3baf5a7bfc8fc0b531c099dd
stevekutz <stkutz@gmail.com>
2020-09-12 11:08:42 -0500

    initial setup copied in

---
+++ ls8/ls8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+cpu = CPU()
+
+if len(sys.argv) != 2:
+    print(f' syntax error with command line ')
+    sys.exit(1)
+
+print(f' >>>> sys.argv    {sys.argv} sys.argv')
+print(f' sys.argv[1] is {sys.argv[1]} ')
+cpu.load(sys.argv[1])
+# cpu.dump_mem('h')
+cpu.run()
