8464e8232e41c481bfe5aa763c486786e5fecffd
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-09-18 09:12:13 -0700

    Added ls8

---
+++ ls8/cpu.py
@@ -0,0 +1,96 @@
+"""CPU functionality."""
+
+import sys
+
+class CPU:
+    """Main CPU class."""
+
+    def __init__(self):
+        """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def load(self):
+        """Load a program into memory."""
+
+        address = 0
+
+        # For now, we've just hardcoded a program:
+
+        program = [
+            # From print8.ls8
+            0b10000010, # LDI R0,8
+            0b00000000,
+            0b00001000,
+            0b01000111, # PRN R0
+            0b00000000,
+            0b00000001, # HLT
+        ]
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
+
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        #elif op == "SUB": etc
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            #self.fl,
+            #self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+
+    def run(self):
+        """Run the CPU."""
+        while True:
+            
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            # local variables
+            LDI = 0b10000010
+            PRN = 0b01000111
+            HALT = 0b00000001
+            if ir is LDI:
+                # reg num is going to have operand:a 
+                # value is number where we can have the number 
+                
+                reg_num = self.reg[operand_a]
+                value = self.reg[operand_b]
+                self.reg[reg_num] = value
+                self.pc + 1
+            
+            elif ir is PRN:
+                print(self.reg[operand_a])
+            
+            elif ir is HALT:
+                exit(1)
+        
