fdd60ed3b72bb689a191f6a5b84359cc8e28508c
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-09-18 10:01:53 -0700

    MVP

---
+++ ls8 /ls8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+
+from cpu import *
+
+cpu = CPU()
+
+if len(sys.argv) < 2:
+    print("ERROR, MUST SPECIFY FILE")
+
+prog_path = sys.argv[1]
+
+cpu.load(prog_path)
+cpu.run()
