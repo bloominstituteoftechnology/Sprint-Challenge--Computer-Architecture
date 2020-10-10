1f0e33aab971c1e2d43952934576789687fe8f5b
Denis Cedeno <denis.cedeno@gmail.com>
2020-10-09 21:45:38 -0400

    initial commit ls8 and cpu code

---
+++ ls8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+
+def main(argv):
+    """Main."""
+
+    if len(argv) != 2:
+        print(f"usage: {argv[0]} filename", file=sys.stderr)
+        return 1
+
+    cpu = CPU()
+
+    cpu.load(argv[1])
+    cpu.run()
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
